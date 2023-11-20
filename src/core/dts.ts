c5a06b4bf7777d62a1cb77f66d166f081f0bd261
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-11-20 14:47:43 +0800

    chore: fix lint

--- src/core/dts.ts
+++ src/core/dts.ts
@@ -2,9 +2,9 @@ import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import { getExportsStatic } from 'pkg-exports'
 import { normalizePath } from '@rollup/pluginutils'
-import { type GlobMap } from '..'
 import { ID_PREFIX } from './constants'
 import { pascalCase } from './utils'
+import type { GlobMap } from '..'
 
 export async function getTypeDeclaration(map: GlobMap, filename: string) {
   function relatePath(filepath: string) {
