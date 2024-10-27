b398856d5f6ac135c2b0785b697116abba6798d4
penjj <32674989+penjj@users.noreply.github.com>
2024-10-27 10:33:38 +0800

    fix: esbuild support (#53)
    Co-authored-by: 三咲智子 Kevin Deng <sxzz@sxzz.moe>

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import path from 'node:path'
+import path, { dirname } from 'node:path'
 import { createFilter, normalizePath } from '@rollup/pluginutils'
 import glob from 'fast-glob'
-import { createUnplugin } from 'unplugin'
+import { createUnplugin, type UnpluginOptions } from 'unplugin'
 import { ID_PREFIX } from './core/constants'
 import { writeTypeDeclaration } from './core/dts'
 import { resolveOption, type Options } from './core/options'
@@ -17,42 +17,48 @@ export default createUnplugin<Options>((options = {}) => {
   const filter = createFilter(opt.include, opt.exclude)
   const map: GlobMap = {}
 
-  return {
+  const resolveId = (id: string, src: string | undefined) => {
+    if (!src || !filter(src)) return
+
+    const [name, pattern] = id.replace(ID_PREFIX, '').split(':', 2)
+    return `${ID_PREFIX}${name}:${src.replaceAll(
+      ':',
+      DRIVER_DIVIDER,
+    )}:${pattern}`
+  }
+
+  const load = async (id: string) => {
+    const [name, src, pattern] = id.replace(ID_PREFIX, '').split(':', 3)
+    const filename = src.replaceAll(DRIVER_DIVIDER_REGEXP, ':')
+
+    const files = (
+      await glob(pattern, {
+        cwd: filename ? path.dirname(filename) : opt.root,
+        absolute: true,
+      })
+    )
+      .map((file) => normalizePath(file))
+      .filter((file) => file !== normalizePath(filename))
+      .sort()
+    map[`${name}:${pattern}`] = files
+
+    const contents = files.map((file) => `export * from '${file}'`).join('\n')
+
+    if (opt.dts) await writeTypeDeclaration(map, opt.dts)
+
+    return `${contents}\n`
+  }
+
+  const context: UnpluginOptions = {
     name,
 
-    resolveId(id, src) {
+    resolveId(id, importer) {
       if (!id.startsWith(ID_PREFIX)) return
-      if (!src || !filter(src)) return
-
-      const [name, pattern] = id.replace(ID_PREFIX, '').split(':', 2)
-      return `${ID_PREFIX}${name}:${src.replaceAll(
-        ':',
-        DRIVER_DIVIDER,
-      )}:${pattern}`
+      return resolveId(id, importer)
     },
-
-    async load(id) {
+    load(id) {
       if (!id.startsWith(ID_PREFIX)) return
-
-      const [name, src, pattern] = id.replace(ID_PREFIX, '').split(':', 3)
-      const filename = src.replaceAll(DRIVER_DIVIDER_REGEXP, ':')
-
-      const files = (
-        await glob(pattern, {
-          cwd: filename ? path.dirname(filename) : opt.root,
-          absolute: true,
-        })
-      )
-        .map((file) => normalizePath(file))
-        .filter((file) => file !== normalizePath(filename))
-        .sort()
-      map[`${name}:${pattern}`] = files
-
-      const contents = files.map((file) => `export * from '${file}'`).join('\n')
-
-      if (opt.dts) await writeTypeDeclaration(map, opt.dts)
-
-      return `${contents}\n`
+      return load(id)
     },
 
     vite: {
@@ -60,5 +66,32 @@ export default createUnplugin<Options>((options = {}) => {
         opt.root = config.root
       },
     },
+
+    esbuild: {
+      setup(build) {
+        build.onResolve(
+          { filter: new RegExp(`^${ID_PREFIX}`) },
+          ({ path, importer }) => {
+            const id = resolveId(path, importer)
+            if (id) return { path: id, namespace: name }
+          },
+        )
+
+        build.onLoad(
+          { filter: new RegExp(`^${ID_PREFIX}`), namespace: name },
+          async ({ path }) => {
+            let [, src] = path.replace(ID_PREFIX, '').split(':', 2)
+            src = src.replaceAll(DRIVER_DIVIDER_REGEXP, ':')
+
+            return {
+              contents: await load(path),
+              resolveDir: dirname(src),
+            }
+          },
+        )
+      },
+    },
   }
+
+  return context
 })
